91da348409cfee37d674dc6f6f9076dcb1b01a4f
Manuel Reiländer <manuel.reilaender@sonorys.at>
2020-05-06 22:01:15 +0200

    change scripts for cross compiling for raspberry pi

--- swig/build-linux-armv7.sh
+++ swig/build-linux-armv7.sh
@@ -10,19 +10,18 @@
 source build-utils.shinc
 check_min_req_vars
 
-export os_arch=armv7
+export os_arch=arm
 export os_build=linux
 export SHARED_LIB=lib${LIBRARY_NAME}.so
 export SHARED_LIB_FINAL=${SHARED_LIB} # dummy for macosx
-export CXX=g++
-prepare_linux_toolchain
-export CC=gcc-5
-export run_openssl_configure="./Configure linux-x86_64 ${OPENSSL_NO_OPTS} -fPIC --prefix=${OPENSSL_ROOT}";
+export CXX=arm-linux-gnueabihf-g++
+export CC=arm-linux-gnueabihf-gcc
+export run_openssl_configure="./Configure linux-generic32 ${OPENSSL_NO_OPTS} -fPIC --prefix=${OPENSSL_ROOT}";
 export run_readelf="readelf -d bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
 export run_bjam="${BOOST_ROOT}/b2 -j8 --user-config=config/${os_build}-${os_arch}-config.jam variant=release toolset=gcc-${os_arch} target-os=${os_build} location=bin/release/${os_build}/${os_arch}"
 export run_strip="strip --strip-unneeded -x bin/release/${os_build}/${os_arch}/${SHARED_LIB}"
 export run_objcopy="objcopy --only-keep-debug bin/release/${os_build}/${os_arch}/${SHARED_LIB} bin/release/${os_build}/{$os_arch}/${SHARED_LIB}.debug"
-sed -i 's/RANLIB = ranlib/RANLIB = "${ANDROID_TOOLCHAIN}\/bin\/i686-linux-android-ranlib"/g' ${BOOST_ROOT}/tools/build/src/tools/gcc.jam;
+#sed -i 's/RANLIB = ranlib/RANLIB = "${ANDROID_TOOLCHAIN}\/bin\/i686-linux-android-ranlib"/g' ${BOOST_ROOT}/tools/build/src/tools/gcc.jam;
 
 create_folder_if_it_doesnt_exist ${SRC}
 prompt_msg "About to prepare BOOST ${BOOST_VERSION}"
